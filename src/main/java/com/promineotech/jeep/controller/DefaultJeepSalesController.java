b8edfe322fea0c6cd32400311908d5ef6ddc56cb
Adam Salt <salt48186@gmail.com>
2023-04-06 09:16:26 -0700

    first commit

---
+++ src/main/java/com/promineotech/jeep/controller/DefaultJeepSalesController.java
@@ -0,0 +1,18 @@
+package com.promineotech.jeep.controller;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.RestController;
+import com.promineotech.jeep.entity.Jeep;
+import com.promineotech.jeep.entity.JeepModel;
+
+
+@RestController
+public class DefaultJeepSalesController implements JeepSalesController {
+
+  @Override
+  public List<Jeep> fetchJeepList(JeepModel model, String trim) {
+    
+    return null;
+  }
+
+}
